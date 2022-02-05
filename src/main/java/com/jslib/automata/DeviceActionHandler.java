2222688d7b5734dca4c26467f3147ca6afde4a7b
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-02-05 06:02:36 +0200

    Update tiny container libraries.

--- src/main/java/com/jslib/automata/DeviceActionHandler.java
+++ src/main/java/com/jslib/automata/DeviceActionHandler.java
@@ -2,5 +2,7 @@ package com.jslib.automata;
 
 public interface DeviceActionHandler
 {
-  Object invokeDeviceAction(String deviceName, String actionName, Object... arguments) throws Exception;
+  
+  Object invokeDeviceAction(String deviceName, String actionName, Object[] arguments) throws Exception;
+  
 }
